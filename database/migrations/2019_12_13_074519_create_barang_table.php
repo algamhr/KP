c273455671ae60ba71e1ab7be9656e046e11e8a7
algamhr <algamahargarika9@gmail.com>
2019-12-13 17:52:18 +0700

    Setup Database

--- database/migrations/2019_12_13_074519_create_barang_table.php
+++ database/migrations/2019_12_13_074519_create_barang_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateBarangTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('barang', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('username');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('kategori_brg');
+            $table->string('nama_brg');
+            $table->integer('jumlah_brg');
+            $table->date('tgl_masuk');
+            $table->date('tgl_keluar');
             $table->timestamps();
         });
     }
@@ -32,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('barang');
     }
 }
