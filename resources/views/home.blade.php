34f78b0c7e7ef38effedfa42130a329d681f22a3
edolorenza <edolrnz@gmail.com>
2020-01-01 15:06:07 +0700

    chart

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -50,7 +50,7 @@
                 <!-- Card Header - Dropdown -->
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                     <h6 class="m-0 font-weight-bold text-primary">Perbandingan Barang</h6>
-                    
+
                 </div>
                 <!-- Card Body -->
                 <div class="card-body">
@@ -64,6 +64,8 @@
                         <span class="mr-2">
                             <i class="fas fa-circle text-info"></i> Barang Keluar
                         </span>
+
+
                     </div>
                 </div>
             </div>
@@ -75,5 +77,55 @@
 
 </div>
 <!-- End of Main Content -->
+
+
+@endsection
+
+@section ('scripts')
+ <!-- Page level plugins -->
+ <script src="{{asset('assets/backend/vendor/chart.js/Chart.min.js')}}"></script>
+
+ <!-- Page level custom scripts -->
+ <script src="{{asset('js/demo/chart-area-demo.js')}}"></script>
+
+
+ <script>
+    // Set new default font family and font color to mimic Bootstrap's default styling
+    Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+    Chart.defaults.global.defaultFontColor = '#858796';
+
+    // Pie Chart Example
+    var ctx = document.getElementById("myPieChart");
+    var myPieChart = new Chart(ctx, {
+      type: 'doughnut',
+      data: {
+        labels: ["Barang Masuk", "Barang Keluar"],
+        datasets: [{
+          data: [{{$total_masuk}}, {{$total_keluar}}],
+          backgroundColor: ['#1cc88a', '#36b9cc'],
+          hoverBackgroundColor: [ '#17a673', '#2c9faf'],
+          hoverBorderColor: "rgba(234, 236, 244, 1)",
+        }],
+      },
+      options: {
+        maintainAspectRatio: false,
+        tooltips: {
+          backgroundColor: "rgb(255,255,255)",
+          bodyFontColor: "#858796",
+          borderColor: '#dddfeb',
+          borderWidth: 1,
+          xPadding: 15,
+          yPadding: 15,
+          displayColors: false,
+          caretPadding: 10,
+        },
+        legend: {
+          display: false
+        },
+        cutoutPercentage: 80,
+      },
+    });
+
+    </script>
 
 @endsection
