1bc0809f97cdb07187099edc9e9a06f0643203cb
algamhr <algamahargarika9@gmail.com>
2020-01-01 15:22:58 +0700

    Home edit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,37 +13,7 @@
     <!-- Content Row -->
 
     <div class="row">
-
-        <!-- Area Chart -->
-        <div class="col-xl-8 col-lg-7">
-            <div class="card shadow mb-4">
-                <!-- Card Header - Dropdown -->
-                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 class="m-0 font-weight-bold text-primary">Daftar Barang</h6>
-                    <div class="dropdown no-arrow">
-                        <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown"
-                            aria-haspopup="true" aria-expanded="false">
-                            <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in"
-                            aria-labelledby="dropdownMenuLink">
-                            <div class="dropdown-header">Dropdown Header:</div>
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                        </div>
-                    </div>
-                </div>
-                <!-- Card Body -->
-                <div class="card-body">
-                    <div class="chart-area">
-                        <canvas id="myAreaChart"></canvas>
-                    </div>
-                </div>
-            </div>
-        </div>
-
+        
         <!-- Pie Chart -->
         <div class="col-xl-4 col-lg-5">
             <div class="card shadow mb-4">
@@ -99,11 +69,11 @@
     var myPieChart = new Chart(ctx, {
       type: 'doughnut',
       data: {
-        labels: ["Barang Masuk", "Barang Keluar"],
+        labels: ["Barang Keluar", "Barang Masuk"],
         datasets: [{
-          data: [{{$total_masuk}}, {{$total_keluar}}],
-          backgroundColor: ['#1cc88a', '#36b9cc'],
-          hoverBackgroundColor: [ '#17a673', '#2c9faf'],
+          data: [{{$total_keluar}}, {{$total_masuk}}],
+          backgroundColor: ['#36b9cc', '#1cc88a'],
+          hoverBackgroundColor: [ '#2c9faf', '#17a673'],
           hoverBorderColor: "rgba(234, 236, 244, 1)",
         }],
       },
